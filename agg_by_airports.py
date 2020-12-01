8f89e09c090d59638ca26440b939042985dbc029
Johnny jo <70656110+xvllinihao@users.noreply.github.com>
2020-12-01 23:33:56 +0100

    first commit

---
+++ agg_by_airports.py
@@ -0,0 +1,86 @@
+import pandas as pd
+
+airlines = pd.read_csv("data/airlines.csv")
+airports = pd.read_csv("data/airports.csv")
+flights = pd.read_csv("data/flights.csv",low_memory=False)
+
+flights_christmas = flights[(flights["MONTH"]==12) & (flights["DAY"]>=18)]
+flights_christmas_delay = flights[(flights["MONTH"]==12) & (flights["DAY"]>=18) & ((flights["ARRIVAL_DELAY"]>0) | (flights["DEPARTURE_DELAY"]>0))]
+
+location_map = {}
+for airport, lon, lat in zip(airports["IATA_CODE"],airports["LONGITUDE"],airports["LATITUDE"]):
+    location_map[airport] = {"lon":lon, "lat":lat}
+
+origins = flights_christmas.groupby(
+    ["ORIGIN_AIRPORT"]) \
+    .size() \
+    .reset_index() \
+    .rename(columns={0: 'SUM'})
+
+origin_delay = flights_christmas_delay.groupby(
+    ["ORIGIN_AIRPORT"]) \
+    .size() \
+    .reset_index() \
+    .rename(columns={0: 'DELAY_SUM'})
+
+origins = pd.merge(origins,origin_delay,on="ORIGIN_AIRPORT",how="outer")
+
+origin_delay_rate = []
+for sum,delay in zip(origins["SUM"],origins["DELAY_SUM"]):
+    if isinstance(delay,float):
+        origin_delay_rate.append(delay/sum)
+    else:
+        origin_delay_rate.append(1)
+longitude = []
+latitude = []
+
+for airport in origins["ORIGIN_AIRPORT"]:
+    longitude.append(location_map[airport]["lon"])
+    latitude.append(location_map[airport]["lat"])
+
+origins["LON"] = longitude
+origins["LAT"] = latitude
+origins["DELAY_RATE"] = origin_delay_rate
+origins.to_json("data/origin.json")
+print(origins)
+
+destinations = flights_christmas.groupby(
+    ["DESTINATION_AIRPORT"]) \
+    .size() \
+    .reset_index() \
+    .rename(columns={0: 'SUM'})
+
+destination_delay = flights_christmas_delay.groupby(
+    ["DESTINATION_AIRPORT"]) \
+    .size() \
+    .reset_index() \
+    .rename(columns={0: 'DELAY'})
+
+destinations = pd.merge(destinations,destination_delay, how="outer", on="DESTINATION_AIRPORT")
+
+destination_delay_rate = []
+for sum,delay in zip(destinations["SUM"],destinations["DELAY"]):
+    if isinstance(delay,int):
+        destination_delay_rate.append(delay/sum)
+    else:
+        destination_delay_rate.append(1)
+
+destinations["LON"] = longitude
+destinations["LAT"] = latitude
+destinations["DELAY_RATE"] = destination_delay_rate
+
+destinations.to_json("data/destination.json")
+print(destinations)
+
+flights_per_day = flights_christmas.groupby(["MONTH", "DAY"]).size().reset_index().rename(columns={0: "SUM"})
+df = pd.DataFrame({
+    'year': [2015]*len(flights_per_day["MONTH"]),
+    'month':flights_per_day["MONTH"],
+    "day": flights_per_day["DAY"]
+})
+
+df = pd.to_datetime(df)
+flights_per_day["DATE"] = df
+flights_per_day = flights_per_day[["DATE","SUM"]]
+flights_per_day.to_json("data/flights_per_day.json")
+print(flights_per_day)
---
+++ convert_iata_code.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+
+df_aircode1 = pd.read_csv('data/L_AIRPORT.csv')
+df_aircode2 = pd.read_csv('data/L_AIRPORT_ID.csv')
+
+df_aircode1 = df_aircode1.reset_index()
+df_aircode2 = df_aircode2.reset_index()
+df_aircodes = pd.merge(df_aircode1,df_aircode2,on='Description')
+aircode_dict = dict(zip(df_aircodes['Code_y'].astype(str),df_aircodes['Code_x']))
+
+# Load data
+df_fl = pd.read_csv('data/flights.csv')
+
+# Make sure all Origin and departing airports are strings
+df_fl['ORIGIN_AIRPORT'] = df_fl['ORIGIN_AIRPORT'].values.astype(str)
+df_fl['DESTINATION_AIRPORT'] = df_fl['DESTINATION_AIRPORT'].values.astype(str)
+
+N_flights = len(df_fl)
+for i in range(N_flights):
+    if i % 100000 == 0:
+        print(i)
+    if len(df_fl['ORIGIN_AIRPORT'][i]) != 3:
+        to_replace = df_fl['ORIGIN_AIRPORT'][i]
+        value = aircode_dict[df_fl['ORIGIN_AIRPORT'][i]]
+        df_fl = df_fl.replace(to_replace, value)
+        print('replaced',to_replace,'with',value)
+    elif len(df_fl['DESTINATION_AIRPORT'][i]) != 3:
+        to_replace = df_fl['DESTINATION_AIRPORT'][i]
+        value = aircode_dict[df_fl['DESTINATION_AIRPORT'][i]]
+        df_fl = df_fl.replace(to_replace, value)
+        print('replaced',to_replace,'with',value)
